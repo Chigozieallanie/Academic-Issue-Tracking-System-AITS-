224e9ea32a836283da49c014bc3ad24df7fe0375
Chigozieallanie <chigozieallanie@gmail.com>
2025-03-14 12:34:31 +0300

    update in the css of the welcome page to reduce conflicts with the dashboard css

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -6,9 +6,9 @@ function Welcome() {
   return (
     <div className="welcome-container">
       {/* Header Section */}
-      <header className="head">
+      <header className="welcome-head">
         <h1>MAK AITS - Your Trusted Academic Issue Tracking System</h1>
-        <div className="auth-buttons">
+        <div className="welcome-auth-buttons">
           <Link to="/signup">
             <button>Sign Up</button>
           </Link>
@@ -19,28 +19,30 @@ function Welcome() {
       </header>
 
       {/* Main Content */}
-      <main className="main-content">
-        <h2>Welcome to MAK-AITS</h2>
-        <p>
-          The <strong>MAK-Academic Issue Tracking System (AITS)</strong> is
-          designed to streamline the process of reporting and managing Academic
-          issues efficiently.Student facing academic issues are helped, AITS
-          ensures seamless communication and resolution.
-        </p>
-        <h2>Our Mission</h2>
-        <p>
-          We aim to provide a seamless and efficient platform for students,
-          lecturers, and the registrar to track and resolve academic issues.
-        </p>
-        <h2>Contact Us</h2>
-        <p>Email: support@academicsystem.com</p>
-        <p>Phone: +256-414-542803</p>
-        <p>Address: 7062 University Road, Kampala, Uganda</p>
-        <p> P.O. Box 7062, Kampala, Uganda</p>
+      <main className="welcome-main-content">
+        <div className="welcome-text">
+          <h2>Welcome to MAK-AITS</h2>
+          <p>
+            The <strong>MAK-Academic Issue Tracking System (AITS)</strong> is
+            designed to streamline the process of reporting and managing
+            Academic issues efficiently. Student facing academic issues are
+            helped, AITS ensures seamless communication and resolution.
+          </p>
+          <h2>Our Mission</h2>
+          <p>
+            We aim to provide a seamless and efficient platform for students,
+            lecturers, and the registrar to track and resolve academic issues.
+          </p>
+          <h2>Contact Us</h2>
+          <p>Email: support@academicsystem.com</p>
+          <p>Phone: +256-414-542803</p>
+          <p>Address: 7062 University Road, Kampala, Uganda</p>
+          <p> P.O. Box 7062, Kampala, Uganda</p>
+        </div>
       </main>
 
       {/* Footer Section */}
-      <footer className="footer">
+      <footer className="welcome-footer">
         <p>© {new Date().getFullYear()} MAK-AITS. All rights reserved.</p>
         <p>Empowering students through seamless issue resolution.</p>
       </footer>
