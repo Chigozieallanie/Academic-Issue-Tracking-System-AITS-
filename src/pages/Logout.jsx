63d2f568addcf63dcf184e3cdc1abba9286ac19c
Chigozieallanie <chigozieallanie@gmail.com>
2025-03-25 13:38:23 +0300

    linked the buttons in the log out page

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
 function Logout() {
   return (
     <div className="logout-container">
       <h2>Are you sure you want to logout?</h2>
-      <button>Cancel</button>
-      <button>Logout</button>
+
+      <Link to="/dashboard">
+        <button>Cancel</button>
+      </Link>
+      <Link to="/login">
+        <button>Logout</button>
+      </Link>
     </div>
   );
 }
