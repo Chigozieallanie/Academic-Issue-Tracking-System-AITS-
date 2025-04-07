5bdf4bb3679380248eceb1d095b51166ac3f7280
Nantale Cissy <cssnantale@gmail.com>
2025-04-07 09:31:35 +0300

    Merge conflicts

--- src/Signupxx/App.js
+++ src/Signupxx/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import About from "./About";
+import LecturerSignup from "./LecturerSignup";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/about" element={<About />} />
+        <Route path ="/LecturerSignup"element={<LecturerSignup/>}/>
       </Routes>
     </Router>
   );
