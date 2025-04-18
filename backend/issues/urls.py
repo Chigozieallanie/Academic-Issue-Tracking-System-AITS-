8ea09e82eb4ce8b9a75bd140ece51d2eed4e633d
Tivah-333 <ainebyoonadativah@gmail.com>
2025-04-18 08:44:50 +0100

    Fix issue detail URL pattern in API routes

--- backend/issues/urls.py
+++ backend/issues/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from .views import IssueListCreateView
 from rest_framework_simplejwt.views import TokenRefreshView
+from .views import IssueRetrieveUpdateDestroy  # Import the view
 
 # RegisterView, LoginView #, IssueDetailView
 from .views import UserRegistrationView, UserLoginView, UserLogoutView, UserProfileView
@@ -13,6 +14,7 @@ urlpatterns = [
     path('login', UserLoginView.as_view(), name='user-login'),
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
     path('issues/', IssueListCreateView.as_view(), name='issue-list-create'),
+    path('issues/<int:pk>/', IssueRetrieveUpdateDestroy.as_view(), name='issue-detail'),  
     path('profile', UserProfileView.as_view(), name='user-profile'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
