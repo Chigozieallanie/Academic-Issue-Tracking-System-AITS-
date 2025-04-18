dacbd6b8765af88dff33373d28487d93f1bad88b
Tivah-333 <ainebyoonadativah@gmail.com>
2025-04-18 20:34:53 +0100

    Setup Django admin panel and registered models

--- backend/issues/admin.py
+++ backend/issues/admin.py
@@ -1,3 +1,20 @@
+
 from django.contrib import admin
+from .models import CustomUser, StudentProfile, LecturerProfile, RegistrarProfile, Issue, Course, Enrollment, Mentorship, Comment, CourseMaterial, Attendance
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(StudentProfile)
+admin.site.register(LecturerProfile)
+admin.site.register(RegistrarProfile)
+admin.site.register(Issue)
+admin.site.register(Course)
+admin.site.register(Enrollment)
+admin.site.register(Mentorship)
+admin.site.register(Comment)
+admin.site.register(CourseMaterial)
+admin.site.register(Attendance)
+
+
+
+
