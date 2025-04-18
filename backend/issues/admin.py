87aca65d5b7cdd02e24c34b0682cc0547034dc01
Tivah-333 <ainebyoonadativah@gmail.com>
2025-04-18 21:49:11 +0100

    set up Django admin and register core models

--- backend/issues/admin.py
+++ backend/issues/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from .models import CustomUser, StudentProfile, LecturerProfile, RegistrarProfile, Issue, Course, Enrollment, Mentorship, Comment, CourseMaterial, Attendance
+from .models import Notification
 
 # Register your models here.
 admin.site.register(CustomUser)
@@ -14,6 +15,7 @@ admin.site.register(Mentorship)
 admin.site.register(Comment)
 admin.site.register(CourseMaterial)
 admin.site.register(Attendance)
+admin.site.register(Notification)
 
 
 
