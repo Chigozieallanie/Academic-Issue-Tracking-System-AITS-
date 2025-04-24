12c8581c54dc78bea747828ae35070b1e8750788
Okedi Ismail <okediismail820@gmail.com>
2025-04-24 22:18:34 +0300

    changes to serializers

--- backend/issues/serializers.py
+++ backend/issues/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, status, viewsets
+from rest_framework import generics, status, viewsets, serializers
 from .models import Issue, StudentProfile as Student, CustomUser, LecturerProfile
 from rest_framework.response import Response
 from rest_framework.views import APIView
