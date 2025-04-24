bfd397482f0accfcd0dd69a713cc566cec763704
Okedi Ismail <okediismail820@gmail.com>
2025-04-24 23:26:43 +0300

    fixed problem in models

--- backend/issues/models(2).py
+++ backend/issues/models(2).py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class Permission(models.Model):
     name = models.CharField(max_length=100, unique=True) 
