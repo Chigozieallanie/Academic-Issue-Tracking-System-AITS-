4dc913483488dfde5b170f78ef6760bd2d741e15
Tivah-333 <ainebyoonadativah@gmail.com>
2025-04-25 21:47:59 +0100

    Removed 'department' field from models and delete corresponding serializer

--- backend/issues/serializers.py
+++ backend/issues/serializers.py
@@ -44,17 +44,14 @@ from .models import (
 User = get_user_model()
 
 from rest_framework import serializers
-from .models import User, Department, Issue
+from .models import User, Issue
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'is_student', 'is_registrar', 'department']
 
-class DepartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Department
-        fields = '__all__'
+
 
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
