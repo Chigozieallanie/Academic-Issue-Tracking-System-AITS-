a9efcf6f3aebf163bcf524dba12210abc9d35f68
Tivah-333 <ainebyoonadativah@gmail.com>
2025-04-25 22:29:11 +0100

    removed enrollment and mentorship to solve error

--- backend/issues/admin.py
+++ backend/issues/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, StudentProfile, LecturerProfile, RegistrarProfile, Issue, Course, Enrollment, Mentorship, Comment, CourseMaterial, Attendance
+from .models import CustomUser, StudentProfile, LecturerProfile, RegistrarProfile, Issue, Course, Comment, CourseMaterial, Attendance
 from .models import Notification
 
 # --- Issue admin customization ---
@@ -37,8 +37,6 @@ admin.site.register(LecturerProfile, LecturerProfileAdmin)
 admin.site.register(RegistrarProfile)
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(Course)
-admin.site.register(Enrollment)
-admin.site.register(Mentorship)
 admin.site.register(Comment)
 admin.site.register(CourseMaterial)
 admin.site.register(Attendance)
