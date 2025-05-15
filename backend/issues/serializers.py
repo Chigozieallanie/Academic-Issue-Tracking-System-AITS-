827e5b6b142ef86f324e20becb4265c851e07f1f
Okedi Ismail <okediismail820@gmail.com>
2025-05-15 20:01:40 +0300

    serializer adjustment

--- backend/issues/serializers.py
+++ backend/issues/serializers.py
@@ -45,6 +45,11 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'role', 'phone_number', 'student_number', 'college')
+        extra_kwargs = {'password': {'write_only': True}}
+
 
 
 
