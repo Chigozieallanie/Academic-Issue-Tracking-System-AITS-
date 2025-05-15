7a5242c5f70ce6b84a7e2e16577dfe22ba000a1a
Okedi Ismail <okediismail820@gmail.com>
2025-05-15 22:50:21 +0300

    serializer adjustments

--- backend/issues/serializers.py
+++ backend/issues/serializers.py
@@ -101,6 +101,11 @@ class IssueSerializer(serializers.ModelSerializer):
         if obj.created_by:
             return f"{obj.created_by.first_name} {obj.created_by.last_name}".strip()
         return None
+    
+    def get_assigned_to_name(self, obj):
+        if obj.assigned_to:
+            return f"{obj.assigned_to.first_name} {obj.assigned_to.last_name}".strip()
+        return None
 
 
 
