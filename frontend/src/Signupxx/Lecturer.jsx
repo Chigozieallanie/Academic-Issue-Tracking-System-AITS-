07391ed7340e76ef71ccacca02a1e1cc48b4aa5d
Chigozieallanie <chigozieallanie@gmail.com>
2025-05-24 10:47:33 +0300

    updates

---
+++ frontend/src/Signupxx/Lecturer.jsx
@@ -0,0 +1,6 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import api from "../../services/api"
+import { useAuth } from "../../contexts/AuthContext"
